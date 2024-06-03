793cefe5d4701018167a2c1d4653a62d469d059d
Dominic Clifton <me@dominicclifton.name>
2024-06-03 16:25:00 +0200

    ReferencePushPullFeederTest: some basic working tests.
    * not many assertions or verified behavior yet, but if the  tests don't throw exceptions that's better than nothing!

--- src/test/java/org/openpnp/machine/reference/feeder/ReferencePushPullFeederTest.java
+++ src/test/java/org/openpnp/machine/reference/feeder/ReferencePushPullFeederTest.java
@@ -3,22 +3,24 @@ package org.openpnp.machine.reference.feeder;
 import com.google.common.io.Files;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.openpnp.machine.reference.ReferenceActuator;
 import org.openpnp.machine.reference.camera.ImageCamera;
-import org.openpnp.model.Configuration;
-import org.openpnp.model.LengthUnit;
-import org.openpnp.model.Location;
-import org.openpnp.model.Tape;
+import org.openpnp.model.*;
 import org.openpnp.spi.*;
+import org.openpnp.util.FeederVisionHelper;
 import org.pmw.tinylog.Configurator;
 import org.pmw.tinylog.Level;
 
 import java.io.File;
+import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class ReferencePushPullFeederTest {
 
     public static final double TEST_PICK_HEAD_OFFSET_Z = -5.0;
@@ -45,13 +47,13 @@ class ReferencePushPullFeederTest {
     // feed direction is to the right (positive X)
     public static final Location LOCATON_0402_STRIP_TOP_RIGHT = new Location(LengthUnit.Millimeters, 80.0, 30.0, 0.0, 0.0);
 
-    public static final Tape TEST_0402_TAPE = new Tape(8.0, 4.0, 2.0, 1.75, 3.5, 2.0);
+    public static final Tape TEST_0402_TAPE = new Tape(8.0, 4.0, 2.0, 1.75, 2.0, 3.5);
 
     private ReferencePushPullFeeder feeder;
     private Nozzle mockedNozzle;
     private Machine machine;
-    private Actuator mockedFeedActuator;
-    private Actuator mockedRotationActuator;
+    private Actuator feedActuator;
+    private Actuator rotationActuator;
     private Head head;
     private ImageCamera camera;
 
@@ -62,6 +64,7 @@ class ReferencePushPullFeederTest {
         buildHead();
         buildDownCamera();
         buildNozzle();
+
         buildActuators();
         buildFeeder();
 
@@ -83,6 +86,7 @@ class ReferencePushPullFeederTest {
             camera = new ImageCamera();
             camera.setName("DOWN");
             camera.setLooking(Camera.Looking.Down);
+            camera.setDefaultZ(new Length(0.0, LengthUnit.Millimeters));
             head.addCamera(camera);
         }
     }
@@ -91,31 +95,54 @@ class ReferencePushPullFeederTest {
         // FIXME using a 'Location' instance set set Image units per pixel, from an API perspective, makes no sense.  Z and rotation are never used. Suggest using a new type without Z and rotation.
         Location imageUnitsPerPixel = new Location(LengthUnit.Millimeters, cameraImage.mm_per_pixel, cameraImage.mm_per_pixel, 0, 0);
         camera.setImageUnitsPerPixel(imageUnitsPerPixel);
+
+        // FIXME what's the difference between UnitsPerPixel and ImageUnitsPerPixel ?
+        camera.setUnitsPerPixel(imageUnitsPerPixel);
         camera.setSourceUri(cameraImage.uri);
     }
 
     private void buildActuators() throws Exception {
-        mockedFeedActuator = mock(Actuator.class);
-        when(mockedFeedActuator.getName()).thenReturn(TEST_FEED_ACTUATOR_NAME);
-        machine.addActuator(mockedFeedActuator);
 
-        mockedRotationActuator = mock(Actuator.class);
-        when(mockedRotationActuator.getName()).thenReturn(TEST_ROTATION_ACTUATOR_NAME);
-        machine.addActuator(mockedRotationActuator);
+        // initially was using mocked actuators, but it's not possible to save the configuration, you just get a stack overflow or some reflection/opens errors
+        // the requirement for being able to save the XML only exists because the vision pipeline wasn't working and need to compare the pipeline XML to find out what's going on
+        if (false) {
+            feedActuator = mock(Actuator.class);
+            when(feedActuator.getName()).thenReturn(TEST_FEED_ACTUATOR_NAME);
+            machine.addActuator(feedActuator);
+
+            rotationActuator = mock(Actuator.class);
+            when(rotationActuator.getName()).thenReturn(TEST_ROTATION_ACTUATOR_NAME);
+            machine.addActuator(rotationActuator);
+        } else {
+            // so instead, we use a TestActuator instance, which can be saved to XML.
+            feedActuator = new TestActuator();
+            feedActuator.setName(TEST_FEED_ACTUATOR_NAME);
+
+            rotationActuator = new TestActuator();
+            rotationActuator.setName(TEST_ROTATION_ACTUATOR_NAME);
+        }
+
+        head.addActuator(feedActuator);
     }
 
     private void buildFeeder() throws Exception {
         feeder = new ReferencePushPullFeeder();
 
-        feeder.setActuator(mockedFeedActuator);
-        feeder.setActuator2(mockedRotationActuator);
+        feeder.setActuator(feedActuator);
+        feeder.setActuator2(rotationActuator);
+
+        List<Part> parts = Configuration.get().getParts();
+        feeder.setPart(parts.get(0));
 
         machine.addFeeder(feeder);
     }
 
-    private void buildNozzle() {
+    private void buildNozzle() throws Exception {
         mockedNozzle = mock(Nozzle.class);
-        when(mockedNozzle.getName()).thenReturn("Test Nozzle");
+        lenient().when(mockedNozzle.getName()).thenReturn("Test Nozzle");
+        lenient().when(mockedNozzle.getHead()).thenReturn(head);
+
+        head.addNozzle(mockedNozzle);
     }
 
     private void buildDefaultConfiguration() throws Exception {
@@ -132,9 +159,8 @@ class ReferencePushPullFeederTest {
         Configuration.get().load();
     }
 
-    private void buildMachine() throws Exception {
+    private void buildMachine() {
         machine = Configuration.get().getMachine();
-        //machine.setHomed(true);
     }
 
     private void prepareMachine() throws Exception {
@@ -144,7 +170,7 @@ class ReferencePushPullFeederTest {
 
     // FIXME sprocket holes really only required if vision is used/enabled.
     @Test
-    public void feedRequiresCalibratedHolesLocations() throws Exception {
+    public void feedRequiresCalibratedHolesLocations() {
         // when
         Exception thrown = assertThrows(Exception.class, () -> feeder.feed(mockedNozzle));
 
@@ -154,18 +180,14 @@ class ReferencePushPullFeederTest {
 
     /**
      * * uses default CalibrationTrigger value
-     * @throws Exception
+     * * doesn't actually feed anything, since there's no push-pull-motion defined
      */
     @Test void feed() throws Exception {
         // given
-        Location pickLocation = TEST_0402_TAPE.pickLocationFromStart(LOCATON_0402_STRIP_TOP_RIGHT);
+        Location pickLocation = calculatePickLocationFromTape(TEST_0402_TAPE, LOCATON_0402_STRIP_TOP_RIGHT, TEST_PICK_HEAD_OFFSET_Z);
 
-        // and an offset to the left, equal to one tape hole, because the start tape hole is cut down the middle and cannot be recognised as a hole
-        pickLocation = pickLocation.derive( pickLocation.getX() - TEST_0402_TAPE.holePitch, null, null, null);
-
-        pickLocation = pickLocation.derive(null, null, TEST_PICK_HEAD_OFFSET_Z, null);
+        // and
         feeder.setLocation(pickLocation);
-
         configureHoleLocations(pickLocation);
 
         // and
@@ -176,15 +198,70 @@ class ReferencePushPullFeederTest {
         machine.getDefaultHead().getDefaultCamera().moveTo(pickLocation);
 
         // when
+        Exception e = feeder.autoSetupPipeline(camera, FeederVisionHelper.PipelineType.CircularSymmetry);
+
+        // then
+        assertNull(e);
+
+        // when
         feeder.feed(mockedNozzle);
 
         // then
-        // TODO
+        // no exception thrown
+    }
+
+    @Test void autoSetup() throws Exception {
+        // given
+        Location pickLocation = calculatePickLocationFromTape(TEST_0402_TAPE, LOCATON_0402_STRIP_TOP_RIGHT, TEST_PICK_HEAD_OFFSET_Z);
+        feeder.setLocation(pickLocation);
+
+        // and
+        useCameraImage(CameraImage.BEFORE_DRAG);
+
+        // and
+        // Configuration.get().save(); // currently disabled as using a mock for the nozzle causing saving to fail.
+
+        // FIXME why do we need to move the camera here?  Without this, the call to `performVisionOperations` from `obtainCalibratedVisionOffset` will be
+        //       using the current head position (0,0) which is obviously wrong for vision calibration.
+        machine.getDefaultHead().getDefaultCamera().moveTo(pickLocation);
+
+        // when
+        feeder.autoSetup();
+
+        // then
+        // no exception thrown
+    }
+
+    private Location calculatePickLocationFromTape(Tape tape, Location topRight, double pick_z_offset) {
+        Location pickLocation = tape.pickLocationFromStart(topRight);
+
+        // need an offset to the left, equal to one component pitch, because the start tape hole is cut down the middle and cannot be recognised as a hole
+        // and because the first component cavity may be cut in two.
+        // also with a non-zero negative Z offset.
+        pickLocation = pickLocation.derive( pickLocation.getX() - tape.partPitch, null, pick_z_offset, null);
+        return pickLocation;
     }
 
     private void configureHoleLocations(Location pickLocation) {
         // feed/pull direction is to the right
         feeder.setHole1Location(TEST_0402_TAPE.getHole1Location(pickLocation));
         feeder.setHole2Location(TEST_0402_TAPE.getHole2Location(pickLocation));
+    }
+
+    public static class TestActuator extends ReferenceActuator {
+        String readValue = "0.0";
+
+        public void setReadValue(String readValue) {
+            this.readValue = readValue;
+        }
+
+        @Override
+        public void actuate(boolean on) throws Exception {
+        }
+
+        @Override
+        public String read() throws Exception {
+            return readValue;
+        }
     }
 }
